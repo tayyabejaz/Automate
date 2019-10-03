57ad0d40acb651a19922839be35113654ffd7550
adnan865 <jbsia.dani@gmail.com>
2019-10-03 15:21:24 +0500

    just added a dao

--- app/src/main/java/com/innovidio/androidbootstrap/db/dao/FeedDao.java
+++ app/src/main/java/com/innovidio/androidbootstrap/db/dao/FeedDao.java
@@ -1,9 +1,34 @@
 package com.innovidio.androidbootstrap.db.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 
 import com.innovidio.androidbootstrap.entity.Feed;
 
+import java.util.List;
+
 @Dao
 public abstract class  FeedDao extends BaseDao<Feed>{
+
+    @Insert
+    public abstract Long insertFeed(Feed note);
+
+    @Query("SELECT * FROM Feed ORDER BY id desc")
+    public abstract LiveData<List<Feed>> fetchAllFeed();
+
+
+    @Query("SELECT * FROM Feed WHERE category =:catagory")
+    public abstract LiveData<Feed> getFeed(int catagory);
+
+
+    @Update
+    public abstract void updateFeed(Feed note);
+
+
+    @Delete
+    public abstract void deleteFeed(Feed note);
 }
